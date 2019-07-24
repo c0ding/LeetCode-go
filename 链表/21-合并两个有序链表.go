c55b0f559425428d8dae5010a80cd06270103682
张逸阳 <zhangyiyanghz@gmail.com>
2019-07-24 18:46:49 +0800

    链表旋转

---
+++ 链表/21-合并两个有序链表.go
@@ -0,0 +1,33 @@
+package main
+
+/*
+将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+示例：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+
+链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+*/
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+
+	if l1 == nil {
+		return l2
+	}
+
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+
+	}
+
+}
--- 链表/listNode.go
+++ 链表/listNode.go
@@ -1,6 +1,45 @@
 package main
 
+/*
+总结：得到链表总长度
+ListNode cursor=head;
+int length=1;
+while(cursor.next!=null)//循环 得到总长度
+{
+cursor=cursor.next;
+length++;
+}
+*/
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+/* 说是循环旋转，但其实本质上是将尾部向前数第K个元素作为头，原来的头接到原来的尾上*/
+
+/*
+可以这样想，头指针指向第k个元素，就是头；
+然后继续遍历找到尾节点，指向原来的头结点。
+这样空间复杂度为O（1）。
+
+
+循环
+
+if node.next == nil
+	尾节点
+	长度 length ++
+
+长度和k 比较，k大，取模
+
+*/
+
+/*
+说是循环旋转，但其实本质上是将尾部向前数第K个元素作为头，
+如果k >  链表长度 , k= k % 链表长度
+原来的头接到原来的尾上
+*/
+
+/*
+利用循环链表，先把链表首尾相连，再找到位置断开循环
+*/
